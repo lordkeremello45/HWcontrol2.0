a85959c971409a8df0346c8ec22fd205c5ec347a
lime <krmadmglu@gmail.com>
2026-09-07 11:09:45 +0300

    test(ci): skip installer initialization in widget test

--- gui_dashboard/test/widget_test.dart
+++ gui_dashboard/test/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hwcontrol_dashboard/installer_main.dart';
 
 void main() {
-  testWidgets('installer wizard renders without starting bridge network', (tester) async {
-    await tester.pumpWidget(const InstallerApp());
+  testWidgets('installer wizard renders without starting bridge network or model download', (tester) async {
+    await tester.pumpWidget(const InstallerApp(initialize: false));
     expect(find.text('HWControl Kurulum Sihirbazı'), findsOneWidget);
   });
 }
