1c1d372f98bed8565bacf769210d6a4b3b14c2d1
lime <krmadmglu@gmail.com>
2026-09-07 10:21:20 +0300

    ci: add Linux distro validation matrix

--- gui_dashboard/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:hwcontrol_dashboard/installer_main.dart';
-
-void main() {
-  testWidgets('installer wizard renders without starting bridge network', (tester) async {
-    await tester.pumpWidget(const InstallerApp());
-    expect(find.text('HWControl Kurulum Sihirbazı'), findsOneWidget);
-  });
-}
